526910487e0ca734c6bf8c59c2ff9a042a4ddf45
filsergeevich <kfilippskikh@mail.ru>
2025-10-31 13:34:27 +0300

    PrettyBoard

--- sudoku.js
+++ sudoku.js
@@ -16,10 +16,26 @@ function isSolved() {
    */
 }
 
-function prettyBoard() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Выводит в консоль/терминал судоку.
-   * Подумай, как симпатичнее его вывести.
-   */
+function prettyBoard(grid) {
+  const horizontalLine = '───────┼───────┼───────';
+
+  for (let row = 0; row < 9; row++) {
+    let line = '';
+    for (let col = 0; col < 9; col++) {
+      const val = grid[row][col];
+      if (val !== null) {
+        line += val.toString().red + ' ';
+      } else {
+        line += '· ';
+      }
+      if (col === 2 || col === 5) line += '│ ';
+    }
+    console.log(line);
+
+    if (row === 2 || row === 5) {
+      console.log(horizontalLine.gray);
+    }
+  }
 }
+
+

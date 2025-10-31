1156a7fb058ae56febfaea64b1eaeba2c4394e03
userdie55 <akulenko.leonid@protonmail.com>
2025-10-31 13:22:54 +0300

    release 0

--- sudoku.js
+++ sudoku.js
@@ -9,11 +9,8 @@ function solve() {
    */
 }
 
-function isSolved() {
-  /**
-   * Принимает игровое поле в том формате, в котором его вернули из функции solve.
-   * Возвращает булевое значение — решено это игровое поле или нет.
-   */
+function isSolved(board) {
+  return !board.flat().includes(null)
 }
 
 function prettyBoard() {
